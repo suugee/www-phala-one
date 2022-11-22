1c5cfae60ec0eaa240e666a2381be96b924af3f0
suugee <sparksure2021@163.com>
2022-11-22 15:02:32 +0800

    update dashboard

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="dashboard-container">
-    <div class="item-panel p-center">Comming soon</div>
+    <div class="item-panel p-center">
+      🔥TotalDelegation：{{ allStakes }} PHA ┆ TotalFreeDelegation：{{
+        allFree
+      }}
+      PHA ┆ 💎TotalReleasing：{{ allReleasingStakes }} PHA ┆
+      TotalWithdrawing：{{ allWithdraw }} PHA
+    </div>
     <div class="analytics-panel p-center">
       <iframe
         name="myiframe"
         id="myrame"
         src="https://mb.phala.network/public/dashboard/d3e95e99-dcd9-4469-ad32-7e5dbd388cc7#bordered=false"
         frameborder="0"
-        align="center"
         width="88%"
-        height="1680px"
+        height="2000px"
         scrolling="no"
       >
       </iframe>
@@ -18,16 +23,41 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "DashBoard",
   components: {},
   data() {
-    return {};
+    return {
+      allStakes: 0,
+      allFree: 0,
+      allReleasingStakes: 0,
+      allWithdraw: 0,
+    };
   },
 
-  mounted() {},
+  mounted() {
+    this.getInfo();
+  },
 
-  methods: {},
+  methods: {
+    getInfo() {
+      axios
+        .get("https://archive.phala.one/api/pool_info")
+        .then((res) => {
+          if (res.status === 200) {
+            this.allStakes = res.data.allStakes;
+            this.allFree = res.data.allFree;
+            this.allReleasingStakes = res.data.allReleasingStakes;
+            this.allWithdraw = res.data.allWithdraw;
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
 };
 </script>
 
@@ -39,8 +69,8 @@ export default {
 .item-panel {
   margin-top: 80px;
   width: 88%;
-  min-height: 120px;
-  line-height: 120px;
+  min-height: 100px;
+  line-height: 100px;
   background-color: #fff;
   text-align: center;
 }
